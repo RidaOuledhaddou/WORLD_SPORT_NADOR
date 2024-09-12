988d38f2868dc9f84a741f5702d4e0d52f16b7ea
RidaOuledhaddou <142796266+RidaOuledhaddou@users.noreply.github.com>
2024-09-12 14:51:19 +0100

    Update Cards.jsx

--- client/src/Components/Dashboard_admin/components_admin/Cards/Cards.jsx
+++ client/src/Components/Dashboard_admin/components_admin/Cards/Cards.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Cards.css';
 import Card from '../Card/Card';
-
+// Card 
 const Cards = ({ cardsData }) => {
   return (
     <div className="Cards">
       {cardsData.map((card, id) => (
-        <div key={id} className="parentContainer"> // Card 
+        <div key={id} className="parentContainer"> 
           <Card
             title={card.title}
             color={card.color}
