59e630ebaa5793f40d61be4f824e3b259b4d98b3
RidaOuledhaddou <142796266+RidaOuledhaddou@users.noreply.github.com>
2024-09-12 14:52:28 +0100

    Update AdminController.php

--- server/app/Http/Controllers/AdminController.php
+++ server/app/Http/Controllers/AdminController.php
@@ -13,13 +13,13 @@ class AdminController extends Controller
         $admin = Admin::where('email', '=', $request->email)->first();
         if($admin){
             if($request->password == $admin->password){
-                $token = bin2hex(random_bytes(32)); // Generate a random token
-                session(['token_admin' => $token]); // Store token in session
+                $token = bin2hex(random_bytes(32)); // Generate a random token ;
+                session(['token_admin' => $token]); // Store token in session ;
     
                 return response()->json([
                     'message' => 'Login Success',
                     'status' => 'success',
-                    'token' => $token // Optionally return the token in response
+                    'token' => $token // Optionally return the token in response ;
                 ], 200);
             }
         }
@@ -28,22 +28,6 @@ class AdminController extends Controller
             'status' => 'failed'
         ], 401);
     }
-    
-    // public function login(Request $request){
-    //     $admin = admin::where('email','=',$request->email)->first();
-    //     if($admin){
-    //         if($request->password==$admin->password){
-    //             return response([
-    //                 'message' => 'Login Success',
-    //                 'status'=>'success'
-    //             ], 200);
-    //         }
-    //     }
-    //     return response([
-    //         'message' => 'The Provided Credentials are incorrect',
-    //         'status'=>'failed'
-    //     ], 401);
-    // }
     
 
     public function show()
